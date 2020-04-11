6584cb4a431bac82e662b3546d34f37032d12d99
Marko Nikolajevic <mrknikolajevic@gmail.com>
2020-04-11 12:00:26 +0200

    Set up all app ui

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div className='progress blue lighten-4'>
+      <div className='indeteterminate blue'> </div>
+    </div>
+  );
+};
+export default Preloader;
